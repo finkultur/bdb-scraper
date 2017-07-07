f59a89920c65e146a38427d1e623038fd786004c
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2017-07-07 15:50:28 +0200

    Passes getUrl() test

--- scraper.py
+++ scraper.py
@@ -1,4 +1,22 @@
+#!/usr/bin/python
+
 import sys
+import requests
+import simplejson as json
+
+searchStr = "var currentFullsizeImage = "
 
 def getUrl(url):
-    return "not implemented"
+
+    r = requests.get(url, stream=True)
+    for l in r.iter_lines():
+        if searchStr in l:
+            data = l[len(searchStr):-1]
+            j = json.loads(data)
+            return j['fullsizeSrc']
+
+    return "not found"
+
+if __name__ == '__main__':
+    print getUrl("http://dayviews.com/finkultur/36850186/")
+
